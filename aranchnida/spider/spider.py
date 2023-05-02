7f6d87d0b1a1e7e093f4165e585ad9710b934751
AbbyEnRedes <abbyenredes@gmail.com>
2023-05-03 00:14:35 +0200

    agregé un ciclo for para recorer la url

--- aranchnida/spider/spider.py
+++ aranchnida/spider/spider.py
@@ -13,26 +13,25 @@ from urllib.parse import urlparse
 def argument():
     parser = argparse.ArgumentParser() #objeto de ArgumentParser
     #añade argumentos al objeto
-    parser.add_argument()
-    parser.add_argument()
-    parser.add_argument()
-    parser.add_argument()
-    parser.add_argument()
-    parser.add_argument()
-    args = parser.parse_args() #analiza los argumentos
+    parser.add_argument('url', help='introduce la web a \'scrapear\'', type=str)
+    parser.add_argument('-r', '--recursive', help='las imagenes se descargaran de forma recursiva', action='store_true')
+    parser.add_argument('-l', '--depth', help='nivel de profundidad para la busqueda y descarga de imagenes', type=int, default=5)
+    parser.add_argument('-p', '--path', help='carpeta donde se descargaran las imagenes',type=str, default='./data/')
+
+    return parser.parse_args() #analiza los argumentos
 
 #peticion web
-req = requests.get(url)
+def scraping(web_site, level):
+	try:
 
-#comprobar que la peticion de 200
-status_code = req.status.get(url)
-if status_code == 200:
-    soup = bs(req.text, 'html.parser') #pasa el contenido HTML a un objeto BS
-    #recorro todas las entradas de la web para extraer las img
-    for i in soup.find_all('img'):
-        print(i)
+		req = requests.get(web_site, timeout=5)
 
-
-
-
-#def download_img(site)
+		#comprobar  la peticion de 200
+		status_code = req.status.get(web_site)
+		if status_code == 200:
+			soup = bs(req.text, 'html.parser') #pasa el contenido HTML a un objeto BS
+			links = soup.find_all('a')#recorro todas las entradas de la web para extraer las img
+			for link in links:
+				print(i)
+	finally:
+		print(error)
