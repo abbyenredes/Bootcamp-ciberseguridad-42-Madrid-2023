c5039e883e92c401be52f2675c37907df27e84ef
abby <126714712+abbyenredes@users.noreply.github.com>
2023-05-23 17:13:09 +0200

    Update spider.py

--- aranchnida/spider/spider.py
+++ aranchnida/spider/spider.py
@@ -1,36 +1,36 @@
-#!/usr/bin/env python3
-
-#librerias
-import requests
-from bs4 import BeautifulSoup as bs
-import os
-import argparse
-from urllib.parse import urlparse
-
-#Funcion para los argumentos
-parser = argparse.ArgumentParser() #objeto de ArgumentParser
-#añade argumentos al objeto
-parser.add_argument('url', help='introduce la web a \'scrapear\'', type=str)
-parser.add_argument('-r', '--recursive', help='las imagenes se descargaran de forma recursiva', action='store_true')
-parser.add_argument('-l', '--depth', help='nivel de profundidad para la busqueda y descarga de imagenes', type=int, default=5)
-parser.add_argument('-p', '--path', help='carpeta donde se descargaran las imagenes',type=str, default='./data/')
-
-args = parser.parse_args() #analiza los argumentos
-
-#peticion web
-def scraping(web_site, level):
-	try:
-
-		req = requests.get(web_site, timeout=5)
-
-		#comprobar  la peticion de 200
-		status_code = req.status.get(web_site)
-		if status_code == 200:
-			soup = bs(req.text, 'html.parser') #pasa el contenido HTML a un objeto BS
-			links = soup.find_all('a')#recorro todas las entradas de la web para extraer las img
-			for link in links:
-				print(i)
-	finally:
-		print(error)
-
-scrapear(url, depth)
+ #!/usr/bin/env python3
+  2
+  3 #libraries
+  4 import requests
+  5 from bs4 import BeautifulSoup as bs
+  6 import os
+  7 import argparse
+  8 from urllib.parse import urlparse
+  9
+ 10 #Function for the arguments
+ 11 parser = argparse.ArgumentParser() #object of ArgumentParser
+ 12 #add arguments to the  object
+ 13 parser.add_argument('url', help='Enter the  web to \'scrape\'', type=str)
+ 14 parser.add_argument('-r', '--recursive', help='Images will be downloaded recursively', action='store_true')
+ 15 parser.add_argument('-l', '--depth', help='Depth level for searching and downlanding images', type=int, default=5)
+ 16 parser.add_argument('-p', '--path', help='Folder where the images will be downloaded',type=str, default='./data/')
+ 17
+ 18 args = parser.parse_args() #Parse the arguments
+ 19
+ 20 #Web request
+ 21 def scraping(web_site, level):
+ 22         try:
+ 23
+ 24                 req = requests.get(web_site, timeout=5)
+ 25
+ 26                 #Check the request
+ 27                 status_code = req.status.get(web_site)
+ 28                 if status_code == 200:
+ 29                         soup = bs(req.text, 'html.parser') #Pass the HTML content to a  BS object
+ 30                         links = soup.find_all('a')#I go through all the entries of the web to extract the img
+ 31                         for link in links:
+ 32                                 print(i)
+ 33         finally:
+ 34                 print(error)
+ 35
+ 36 scrapear(url, depth)
